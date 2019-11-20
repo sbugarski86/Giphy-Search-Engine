cdcd14c12ac9b3dd07ef46488396796b17ff947e
Stevan Bugarski <bugarski.stevan@gmail.com>
2019-11-20 18:38:29 -0500

    If  else method added to JQuery code

--- src/script.js
+++ src/script.js
@@ -1,41 +1,50 @@
+
 $(function () {
-  $("button")
+  $('button')
     .first()
-    .click(function (e) {
-      e.preventDefault()
-      var $key = "&api_key=gpGF7pIGGMBYiHBnsurbMCEPJ3coTrxL";
-      var $url = "https://api.giphy.com/v1/gifs/search?&";
-      var $value = "q=" + $("#value").val();
-      var $numValue = "&limit=" + $("#number-value").val();
-      var giphySearch = $url + $value + $numValue + $key;
-      $.getJSON(giphySearch)
-        .done(function (result) {
-          for (var i = 0; i < result.data.length; i++) {
-            $("<img class='section-giphy__img'>")
-              .attr("src", result.data[i].images.downsized.url)
-              .appendTo("#giphy");
-          }
-        })
-        .fail(function
-          () {
-          // failure
-          alert("Ajax call failed.");
-        });
+    .click(function () {
+      var $key = '&api_key=gpGF7pIGGMBYiHBnsurbMCEPJ3coTrxL';
+      var $url = 'https://api.giphy.com/v1/gifs/search?&';
+      var $value = $('#value').val();
+      var $numValue = parseInt($('#number-value').val());
+
+      if ($value !== '') {
+        console.log('Stevan');
+        if (isNaN($numValue) || $numValue < 1) {
+          alert('Please enter a number larger then 0');
+        } else {
+          var giphySearch = $url + 'q=' + $value + '&limit=' + $numValue + $key;
+          $.getJSON(giphySearch)
+            .done(function (result) {
+              for (var i = 0; i < result.data.length; i++) {
+                $("<img class='section-giphy__img'>")
+                  .attr('src', result.data[i].images.downsized.url)
+                  .appendTo('#giphy');
+              }
+            })
+            .fail(function () {
+              // failure
+              alert('Ajax call failed.');
+            });
+        }
+      } else {
+        alert('please enter something');
+      }
     });
-  $("button")
+  $('button')
     .last()
     .click(function () {
-      $("#giphy").empty();
+      $('#giphy').empty();
     });
-  $(".fa-arrow-up").hide();
-  $(".fa-arrow-up").click(function () {
-    $(".section-toggle__links").fadeOut(500);
+  $('.fa-arrow-up').hide();
+  $('.fa-arrow-up').click(function () {
+    $('.section-toggle__links').fadeOut(500);
     $(this).hide();
-    $(".fa-bars").show(500);
+    $('.fa-bars').show(500);
   });
-  $(".fa-bars").click(function () {
-    $(".section-toggle__links").fadeTo(500, 1);
+  $('.fa-bars').click(function () {
+    $('.section-toggle__links').fadeTo(500, 1);
     $(this).hide(500);
-    $(".fa-arrow-up").show(500);
+    $('.fa-arrow-up').show(500);
   });
 });
